eec2c95f2b6722a7830291e86d55ab1fe1f992db
Alexander Valdez <alex.valdezp22@gmail.com>
2023-07-26 08:22:50 -0500

    Update EDA Basketball

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Exploratory Data Analysis Basketball
+Veamos ahora el uso de web scrapying para recopilar data de una web y
+publicar de manera efectiva el analisis y resultados con streamlit.
+
---
+++ edaBasketball-data.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+st.title("NBA Player Stats Explorer")
+
+st.markdown("""
+This apps perfoms simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data Source     :** [Basketball-reference.com](https://www.basketball-reference.com/).
+
+""")
+
+st.sidebar.header('User Input Features')
+
+selected_year = st.sidebar.selectbox('Year',list(reversed(range(1950,2020))))
+
+
+# Web Scrapying of NBA players stats
